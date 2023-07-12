f83b22adf24b2e835b4aa351c61667c060550f6b
Seibi0928 <nanteneus@hotmail.co.jp>
2023-07-13 00:24:01 +0900

    :sparkles: printk関数を実装

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -11,6 +11,20 @@ void* operator new(size_t size, void* buf) { return buf; }
 
 void operator delete(void* obj) noexcept {}
 
+char console_buf[sizeof(Console)];
+Console* console;
+
+int printk(const char* format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    char s[1024];
+    int result = vsprintf(s, format, ap);
+    va_end(ap);
+
+    console->PutString(s);
+    return result;
+}
+
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
     PixelWriter* pixel_writer;
@@ -25,12 +39,13 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
             break;
     }
 
-    Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+    console =
+        new (console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
     char buf[128];
     for (int i = 0; i < 100; ++i) {
         sprintf(buf, "line %d\n", i);
-        console.PutString(buf);
+        console->PutString(buf);
     }
     while (1) __asm__("hlt");
 }
