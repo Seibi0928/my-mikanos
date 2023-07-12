947dcab76391de2112e6591fb660f2bef229990c
Seibi0928 <nanteneus@hotmail.co.jp>
2023-07-12 23:36:09 +0900

    :sparkles: 文字列出力処理を実装

--- kernel/font.hpp
+++ kernel/font.hpp
@@ -6,3 +6,5 @@
 
 void WriteAscii(PixelWriter& writer, int x, int y, char c,
                 const PixcelColor& color);
+void WriteString(PixelWriter& writer, int x, int y, const char* s,
+                 const PixcelColor& color);
