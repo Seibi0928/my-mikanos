a1ed4ccd1347e2cb559087c71ecb2dd140ec386c
Seibi0928 <nanteneus@hotmail.co.jp>
2023-07-15 23:47:10 +0900

    :recycle: 割り込み処理を高速化

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -11,6 +11,7 @@
 #include "logger.hpp"
 #include "mouse.hpp"
 #include "pci.hpp"
+#include "queue.hpp"
 #include "usb/classdriver/mouse.hpp"
 #include "usb/device.hpp"
 #include "usb/memory.hpp"
@@ -65,13 +66,16 @@ void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
 
 usb::xhci::Controller* xhc;
 
+struct Message {
+    enum Type {
+        kInterruptXHCI,
+    } type;
+};
+
+ArrayQueue<Message>* main_queue;
+
 __attribute__((interrupt)) void IntHandlerXHCI(InterruptFrame* frame) {
-    while (xhc->PrimaryEventRing()->HasFront()) {
-        if (auto err = ProcessEvent(*xhc)) {
-            Log(kError, "Error while ProcessEvent: %s at %s:%d\n", err.Name(),
-                err.File(), err.Line());
-        }
-    }
+    main_queue->Push(Message{Message::kInterruptXHCI});
     NotifyEndOfInterrupt();
 }
 
@@ -109,6 +113,10 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     char mouse_cursor_buf[sizeof(MouseCursor)];
     mouse_cursor = new (mouse_cursor_buf)
         MouseCursor{pixel_writer, kDesktopBGColor, {300, 200}};
+
+    std::array<Message, 32> main_queue_data;
+    ArrayQueue<Message> main_queue{main_queue_data};
+    ::main_queue = &main_queue;
 
     auto err = pci::ScanAllBus();
     Log(kDebug, "ScanAllBus: %s\n", err.Name());
@@ -167,7 +175,6 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     xhc.Run();
 
     ::xhc = &xhc;
-    __asm__("sti");
 
     usb::HIDMouseDriver::default_observer = MouseObserver;
 
@@ -184,7 +191,28 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
         }
     }
 
-    while (1) __asm__("hlt");
+    while (true) {
+        __asm__("cli");
+        if (main_queue.Count() == 0) {
+            __asm__("sti\n\thlt");
+            continue;
+        }
+
+        auto msg = main_queue.Front();
+        main_queue.Pop();
+        __asm__("sti");
+
+        switch (msg.type) {
+            case Message::kInterruptXHCI:
+                while (auto err = ProcessEvent(xhc)) {
+                    Log(kError, "Error while ProcessEvent: %s at %s:%d\n",
+                        err.Name(), err.File(), err.Line());
+                }
+                break;
+            default:
+                Log(kError, "Unknown message type: %d\n", msg.type);
+        }
+    }
 }
 
 extern "C" void __cxa_pure_virtual() {
