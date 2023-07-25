db4e5a4b42174ee6f47bf95a0bfcc1250e2439e3
Seibi0928 <nanteneus@hotmail.co.jp>
2023-07-25 22:41:21 +0900

    :bug: マウスが画面外に収まるように修正

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -50,8 +50,11 @@ Vector2D<int> screen_size;
 Vector2D<int> mouse_position;
 
 void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
-    layer_manager->MoveRelative(mouse_layer_id,
-                                {displacement_x, displacement_y});
+    auto new_pos =
+        mouse_position + Vector2D<int>{displacement_x, displacement_y};
+    new_pos = ElementMin(new_pos, screen_size + Vector2D<int>{-1, -1});
+    mouse_position = ElementMax(new_pos, {0, 0});
+    layer_manager->Move(mouse_layer_id, mouse_position);
     layer_manager->Draw();
 }
 
@@ -239,10 +242,10 @@ extern "C" void KernelMainNewStack(
         }
     }
 
-    const int kFrameWidth = frame_buffer_config.horizontal_resolution;
-    const int kFrameHeight = frame_buffer_config.vertical_resolution;
+    screen_size.x = frame_buffer_config.horizontal_resolution;
+    screen_size.y = frame_buffer_config.vertical_resolution;
 
-    auto bgwindow = std::make_shared<Window>(kFrameWidth, kFrameHeight,
+    auto bgwindow = std::make_shared<Window>(screen_size.x, screen_size.y,
                                              frame_buffer_config.pixel_format);
     auto bgwriter = bgwindow->Writer();
 
@@ -267,8 +270,10 @@ extern "C" void KernelMainNewStack(
 
     auto bglayer_id =
         layer_manager->NewLayer().SetWindow(bgwindow).Move({0, 0}).ID();
-    mouse_layer_id =
-        layer_manager->NewLayer().SetWindow(mouse_window).Move(mouse_position).ID();
+    mouse_layer_id = layer_manager->NewLayer()
+                         .SetWindow(mouse_window)
+                         .Move(mouse_position)
+                         .ID();
 
     layer_manager->UpDown(bglayer_id, 0);
     layer_manager->UpDown(mouse_layer_id, 1);
