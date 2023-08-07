91055daced910ab8e794bdab39533d3cdbbf7199
Seibi0928 <nanteneus@hotmail.co.jp>
2023-08-07 11:43:59 +0900

    :sparkles: ターミナルウィンドウを実装

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -26,6 +26,7 @@
 #include "pci.hpp"
 #include "segment.hpp"
 #include "task.hpp"
+#include "terminal.hpp"
 #include "timer.hpp"
 #include "usb/classdriver/mouse.hpp"
 #include "usb/device.hpp"
@@ -77,7 +78,7 @@ void InitializeTextWindow() {
     text_window_layer_id = layer_manager->NewLayer()
                                .SetWindow(text_window)
                                .SetDraggable(true)
-                               .Move({350, 200})
+                               .Move({500, 200})
                                .ID();
 
     layer_manager->UpDown(text_window_layer_id,
@@ -202,7 +203,7 @@ extern "C" void KernelMainNewStack(
     InitializeTextWindow();
     InitializeTaskBWindow();
     layer_manager->Draw({{0, 0}, ScreenSize()});
-    
+
     acpi::Initialize(acpi_table);
     InitializeLAPICTimer();
 
@@ -218,6 +219,9 @@ extern "C" void KernelMainNewStack(
 
     const uint64_t taskb_id =
         task_manager->NewTask().InitContext(TaskB, 45).Wakeup().ID();
+
+    const uint64_t task_terminal_id =
+        task_manager->NewTask().InitContext(TaskTerminal, 0).Wakeup().ID();
 
     usb::xhci::Initialize();
     InitializeKeyboard();
@@ -261,6 +265,10 @@ extern "C" void KernelMainNewStack(
                     textbox_cursor_visible = !textbox_cursor_visible;
                     DrawTextCursor(textbox_cursor_visible);
                     layer_manager->Draw(text_window_layer_id);
+
+                    __asm__("cli");
+                    task_manager->SendMessage(task_terminal_id, *msg);
+                    __asm__("sti");
                 }
                 break;
             case Message::kKeyPush:
