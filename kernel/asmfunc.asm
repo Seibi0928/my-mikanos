8d0bfd00d8f12484fb5703434dc2fe9f834a2fac
Seibi0928 <nanteneus@hotmail.co.jp>
2023-08-15 00:11:05 +0900

    :sparkles: キー入力読み取りシステムコールを実装

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -265,6 +265,7 @@ WriteMSR:
     wrmsr
     ret
 
+extern GetCurrentTaskOSStackPointer
 extern syscall_table
 global SyscallEntry
 SyscallEntry:
@@ -277,6 +278,22 @@ SyscallEntry:
     mov rcx, r10
     and eax, 0x7fffffff
     mov rbp, rsp
+
+    ; NOTE: システムコールをOS用スタックで実行するための準備
+    and rsp, 0xfffffffffffffff0
+    push rax
+    push rdx
+    cli
+    call GetCurrentTaskOSStackPointer
+    sti
+    mov rdx, [rsp + 0]  ; RDX
+    mov [rax - 16], rdx
+    mov rdx, [rsp + 8]  ; RAX
+    mov [rax - 8], rdx
+
+    lea rsp, [rax - 16]
+    pop rdx
+    pop rax
     and rsp, 0xfffffffffffffff0
 
     call [syscall_table + 8 * eax]
--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -6,8 +6,10 @@
 #include <cstdint>
 #include <cstring>
 
+#include "app_event.hpp"
 #include "asmfunc.h"
 #include "font.hpp"
+#include "keyboard.hpp"
 #include "logger.hpp"
 #include "msr.hpp"
 #include "task.hpp"
@@ -74,6 +76,9 @@ SYSCALL(OpenWindow) {
                               .Move({x, y})
                               .ID();
     active_layer->Activate(layer_id);
+
+    const auto task_id = task_manager->CurrentTask().ID();
+    layer_task_map->insert(std::make_pair(layer_id, task_id));
     __asm__("sti");
 
     return {layer_id, 0};
@@ -172,19 +177,90 @@ SYSCALL(WinDrawLine) {
         },
         arg1, arg2, arg3, arg4, arg5, arg6);
 }
+
+SYSCALL(CloseWindow) {
+    const unsigned int layer_id = arg1 & 0xffffffff;
+    const auto layer = layer_manager->FindLayer(layer_id);
+
+    if (layer == nullptr) {
+        return {EBADF, 0};
+    }
+
+    const auto layer_pos = layer->GetPosition();
+    const auto win_size = layer->GetWindow()->Size();
+
+    __asm__("cli");
+    active_layer->Activate(0);
+    layer_manager->RemoveLayer(layer_id);
+    layer_manager->Draw({layer_pos, win_size});
+    layer_task_map->erase(layer_id);
+    __asm__("sti");
+
+    return {0, 0};
+}
+
+SYSCALL(ReadEvent) {
+    if (arg1 < 0x8000'0000'0000'0000) {
+        return {0, EFAULT};
+    }
+    const auto app_events = reinterpret_cast<AppEvent*>(arg1);
+    const size_t len = arg2;
+
+    __asm__("cli");
+    auto& task = task_manager->CurrentTask();
+    __asm__("sti");
+    size_t i = 0;
+
+    while (i < len) {
+        __asm__("cli");
+        auto msg = task.ReceiveMessage();
+        if (!msg && i == 0) {
+            task.Sleep();
+            continue;
+        }
+        __asm__("sti");
+
+        if (!msg) {
+            break;
+        }
+
+        switch (msg->type) {
+            case Message::kKeyPush:
+                if (msg->arg.keyboard.keycode == 20 /* Q key */ &&
+                    msg->arg.keyboard.modifier &
+                        (kLControlBitMask | kRControlBitMask)) {
+                    app_events[i].type = AppEvent::kQuit;
+                    ++i;
+                }
+                break;
+            default:
+                Log(kInfo, "uncaught event type: %u\n", msg->type);
+        }
+    }
+
+    return {i, 0};
+}
 #undef SYSCALL
 }  // namespace syscall
 
 using SyscallFuncType = syscall::Result(uint64_t, uint64_t, uint64_t, uint64_t,
                                         uint64_t, uint64_t);
-extern "C" std::array<SyscallFuncType*, 9> syscall_table{
-    syscall::LogString,      syscall::PutString,      syscall::Exit,
-    syscall::OpenWindow,     syscall::WinWriteString, syscall::WinFillRectangle,
-    syscall::GetCurrentTick, syscall::WinRedraw,      syscall::WinDrawLine,
+extern "C" std::array<SyscallFuncType*, 0xb> syscall_table{
+    /* 0x00 */ syscall::LogString,
+    /* 0x01 */ syscall::PutString,
+    /* 0x02 */ syscall::Exit,
+    /* 0x03 */ syscall::OpenWindow,
+    /* 0x04 */ syscall::WinWriteString,
+    /* 0x05 */ syscall::WinFillRectangle,
+    /* 0x06 */ syscall::GetCurrentTick,
+    /* 0x07 */ syscall::WinRedraw,
+    /* 0x08 */ syscall::WinDrawLine,
+    /* 0x09 */ syscall::CloseWindow,
+    /* 0x0a */ syscall::ReadEvent,
 };
 
 void InitializeSyscall() {
-    WriteMSR(kIA32_EFER, 0x051u);
+    WriteMSR(kIA32_EFER, 0x0501u);
     WriteMSR(kIA32_LSTAR, reinterpret_cast<uint64_t>(SyscallEntry));
     WriteMSR(kIA32_STAR, static_cast<uint64_t>(8) << 32 |
                              static_cast<uint64_t>(16 | 3) << 48);
